09e8088571b566ffeab34868bb02728b18f20455
mujtaba <mujtabasadat07800@gmail.com>
2023-01-31 13:30:16 +0430

    make the navbar links smooth

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 'use strict';
-
-///////////////////////////////////////
-// Modal window
-
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+///////////////////////////////////////
+// Modal window
 
 const openModal = function (e) {
   e.preventDefault();
@@ -31,15 +31,47 @@ document.addEventListener('keydown', function (e) {
   }
 });
 
+btnScrollTo.addEventListener('click', function (e) {
+  const s1coords = section1.getBoundingClientRect();
+  console.log(s1coords);
+  console.log(e.target.getBoundingClientRect());
+  console.log('Current Scroll (X/Y)', window.pageXOffset, window.pageYOffset);
+  console.log(
+    'Height/Width viewport',
+    document.documentElement.clientHeight,
+    document.documentElement.clientWidth
+  );
+  // Scrolling
+  // window.scrollTo(
+  //   s1coords.left + window.pageXOffset,
+  //   s1coords.top + window.pageYOffset
+  // );
+  // window.scrollTo({
+  //   left: s1coords.left + window.pageXOffset,
+  //   top: s1coords.top + window.pageYOffset,
+  //   behavior: 'smooth',
+  // });
+  // new way of scrolling
+  section1.scrollIntoView({ behavior: 'smooth' });
+});
+/////////////////////////
+// Page Navigation
+document.querySelectorAll('.nav__link').forEach(function (el) {
+  el.addEventListener('click', function (e) {
+    e.preventDefault();
+    const id = this.getAttribute('href');
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+  });
+});
 ///////////////////////
 ///////////////////////////
-
+/* 
 // Selecting elements
 console.log(document.documentElement);
 console.log(document.head);
 console.log(document.body);
 
-document.querySelector('.header');
+const header = document.querySelector('.header');
 const allSections = document.querySelectorAll('.section');
 console.log(allSections);
 
@@ -53,4 +85,121 @@ console.log(document.getElementsByClassName('btn'));
 /// .insertAdjacentHTML()
 const message = document.createElement('div');
 message.classList.add('cookie-message');
-message.textContent = 'we use cookies for improved functionality and analytics';
+// message.textContent = 'we use cookies for improved functionality and analytics';
+message.innerHTML = `we use cookies for improved functionality and analytics <button class='btn btn--close-cookie'>Got it!</button>`;
+
+// header.prepend(message);
+header.append(message);
+// header.append(message.cloneNode(true));
+
+// header.before(message);
+// header.after(message);
+
+// Delete elements
+document
+  .querySelector('.btn--close-cookie')
+  .addEventListener('click', function () {
+    // new way of removing by using remove() function
+    // message.remove();
+    // classic way of removing
+    message.parentElement.removeChild(message);
+  });
+
+// Styles
+message.style.backgroundColor = '#37383d';
+message.style.width = '120%';
+
+console.log(message.style.color);
+console.log(message.style.backgroundColor);
+
+console.log(getComputedStyle(message).height);
+console.log(getComputedStyle(message).color);
+
+message.style.height =
+  Number.parseFloat(getComputedStyle(message).height, 10) + 30 + 'px';
+
+document.documentElement.style.setProperty('--color-primary', 'orangered');
+
+// Attributes
+const logo = document.querySelector('.nav__logo');
+console.log(logo.alt);
+console.log(logo.src);
+console.log(logo.className);
+
+logo.alt = 'beautiful minimalist logo';
+// Non-standard
+console.log(logo.designer);
+console.log(logo.getAttribute('designer'));
+logo.setAttribute('company', 'bankist');
+
+console.log(logo.src);
+console.log(logo.getAttribute('src'));
+
+const link = document.querySelector('.nav__link--btn');
+console.log(link.href);
+console.log(link.getAttribute('href'));
+
+// Data Attributes
+// Data Attributes are special attribute that's start with a word data
+console.log(logo.dataset.versionNumber);
+
+// Classes
+logo.classList.add('c', 'j');
+logo.classList.remove('c', 'j');
+logo.classList.toggle('c');
+logo.classList.contains('c');
+
+// Don't use
+// logo.className = 'jonas';
+ */
+
+// ----
+const h1 = document.querySelector('h1');
+const alerth1 = function (e) {
+  alert('addeventlistener: Great! You are reading the heading');
+};
+h1.addEventListener('mouseenter', alerth1);
+setTimeout(() => h1.removeEventListener('mouseenter', alerth1), 3000);
+// Old school way to listen events is onevent property
+// h1.onmouseenter = function (e) {
+//   alert('addeventlistener: Great! You are reading the heading');
+// };
+
+// rgb(255,255,255)
+const randonInt = (min, max) =>
+  Math.floor(Math.random() * (max - min + 1) + min);
+
+const randomColor = () =>
+  `rgb(${randonInt(0, 255)},${randonInt(0, 255)},${randonInt(0, 255)})`;
+console.log(randomColor(0, 255));
+console.log(randomColor(0, 255));
+console.log(randomColor(0, 255));
+
+// document.querySelector('.nav__link').addEventListener('click', function (e) {
+//   this.style.backgroundColor = randomColor();
+//   console.log('link', e.target, e.currentTarget);
+//   console.log(e.currentTarget === this);
+
+//   // Stop the propagation by using the propagation() method
+//   e.stopPropagation();
+// });
+// document.querySelector('.nav__links').addEventListener('click', function (e) {
+//   this.style.backgroundColor = randomColor();
+//   console.log('container', e.target, e.currentTarget);
+// });
+// document.querySelector('.nav').addEventListener('click', function (e) {
+//   this.style.backgroundColor = randomColor();
+//   console.log('nav', e.target, e.currentTarget);
+// });
+
+const alex = {
+  age: 18,
+  country: 'canada',
+  printAge: () => {
+    console.log(`Age: ${this.age}`);
+  },
+};
+
+const peter = alex;
+peter.age = 27;
+peter.printAge();
